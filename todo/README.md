4f08b1accfeaff20cd6daf86a00a8404cba708ef
shuaxin <986384495@qq.com>
2020-03-13 20:48:11 +0800

    更新目录

---
+++ todo/README.md
@@ -0,0 +1,33 @@
+# todo
+没有数据库参与的一个todo的前后端分离的demo
+
+# 项目功能说明
+- 增加项目
+- 删除项目
+- 修改存在的项目
+- 显示今天日期
+
+# 目录说明
+- css+js+html为前端部分
+- until下的文件是负责静态资源文件处理的
+- route下的是数据接口
+- indes.js入口文件
+- img存放的前端效果图
+
+# 运行
+```shell
+# 进入根目录安装依赖
+npm i
+# 进入该项目目录运行项目
+node index.js
+# 打开浏览器输入 http://localhost:3000/index.html
+```
+# 界面
+> 主界面
+- ![主界面](./img/one.png)
+>添加项目
+- ![添加项目](./img/two.png)
+>没有项目的界面
+- ![空界面](./img/three.png)
+
+
---
+++ todo/until/resStatic.js
@@ -0,0 +1,57 @@
+// 解析URL
+const parase = require('url').parse
+// 截取路径片段
+const join = require('path').join
+const fs = require('fs')
+// 静态文件的根路径
+const root = __dirname
+// 静态文件服务器
+const resFile = (req, res) => {
+    if (req.url !== "/favicon.ico") {
+        // 获取访问文件的尾缀 设置响应类型
+        let arr = req.url.split('.')
+        let type = arr[arr.length - 1]
+        // 获取访问路径 
+        const url = parase(req.url)
+        // 获取本地路径
+        const path = join(root, '../' + type + url.pathname)
+        // 检查文件是否存在
+        fs.stat(path, (err, stat) => {
+            if (err) {
+                if ('ENOENT' == err.code) {
+                    // 文件不粗才能在
+                    res.statusCode = 404
+                    res.end('Not Found')
+                } else {
+                    // 其他错误
+                    res.statusCode = 500
+                    res.end('internal server error')
+                }
+            } else {
+                // 设置响应文件类型
+                res.setHeader('Content-Type', `text/${type}`)
+                // 设置响应文件大小
+                res.setHeader('Content-Length', stat.size)
+                // 创建fs.ReadStream
+                const stream = fs.createReadStream(path)
+                // 管道优化数据传递 把读取的数据发送到另一个地方 内部自动结束响应  req也可以接
+                stream.pipe(res)
+                // 处理错误  不存在的文件或者访问路径错误
+                stream.on('error', err => {
+                    res.statusCode = 500
+                    res.end('internal server error')
+                })
+            }
+        })
+    }
+}
+// 不存在api响应
+const resErr = (res) => {
+    // 其他错误
+    res.statusCode = 500
+    res.end('internal server error')
+}
+module.exports = {
+    resFile,
+    resErr
+}
